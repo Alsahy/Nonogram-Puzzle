e0219dfa43d221af8770b827c410c6984928a3d9
Alsahy <131991351+Alsahy@users.noreply.github.com>
2024-05-24 23:15:22 +0300

    Adding print_lines

--- draw_functions.py
+++ draw_functions.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 White = (255, 255, 255)
 Black = (0, 0, 0)
+Line_offset = 25
 
 def print_rectangels(img, offset, cell_unit, N, solution):
     """
@@ -22,11 +23,11 @@ def print_rectangels(img, offset, cell_unit, N, solution):
                                 -1) # fill if -1 and line width if another positive value
 
             if solution[row][col] == 1:
-              img = cv2.rectangle(img,
-                                  (offset+col*cell_unit, offset+row*cell_unit),
-                                  (offset+(col+1)*cell_unit, offset+(row+1)*cell_unit),
-                                  White,
-                                  3)
+              img = cv2.rectangle(img, # the image object
+                                  (offset+col*cell_unit, offset+row*cell_unit), # start position
+                                  (offset+(col+1)*cell_unit, offset+(row+1)*cell_unit), # end position
+                                  White, # color
+                                  3) # fill if -1 and line width if another positive value
     return img
 
 def print_lines(img, offset, cell_unit, N, solution):
@@ -39,7 +40,43 @@ def print_lines(img, offset, cell_unit, N, solution):
         :param solution: the sent solution
         :return: img: the final object of the image
     """
+    size = 750
+    for row in range(N + 1):
+        img = cv2.line(img,  # the image object
+                       (offset + row * cell_unit, Line_offset),  # start position
+                       (offset + row * cell_unit, size),  # end position
+                       Black,  # color
+                       2 if (row == N) else 1)
 
+    for col in range(N + 1):
+        img = cv2.line(img,  # the image object
+                       (Line_offset, offset + col * cell_unit),  # start position
+                       (size, offset + col * cell_unit),  # end position
+                       Black,  # color
+                       2 if (col == N) else 1)  # fill if -1 and line width if another positive value
+
+    img = cv2.line(img,  # the image object
+                   (offset, Line_offset),  # start position
+                   (offset, size),  # end position
+                   Black,  # color
+                   2)
+    img = cv2.line(img,  # the image object
+                   (Line_offset, offset),  # start position
+                   (size, offset),  # end position
+                   Black,  # color
+                   2)
+    # first col
+    img = cv2.line(img,  # the image object
+                   (Line_offset, offset),  # start position
+                   (Line_offset, size),  # end position
+                   Black,  # color
+                   2)
+    # first row
+    img = cv2.line(img,  # the image object
+                   (offset, Line_offset),  # start position
+                   (size, Line_offset),  # end position
+                   Black,  # color
+                   2)
     return img
 
 def print_row_clues(img, offset, cell_unit, N, solution, row_clues):
@@ -73,7 +110,7 @@ def print_col_clues(img, offset, cell_unit, N, solution, col_clues):
         for j in range(len(col_clues[i])):
           cv2.putText(img, # image object
                       str(col_clues[i][j]),
-                      (offset+i*cell_unit+int(cell_unit/2), Text_offset * (j+1)), # start position (horizontal offset + cell offset + centering offset, vertical offset)
+                      (offset+i*cell_unit+int(cell_unit/2), Line_offset+Text_offset*(j+1)), # start position(horizontal offset+cell offset+centering offset, Line offset+vertical offset)
                       font, # font type
                       6/N, # size
                       Black, # color
